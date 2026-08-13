3add273ab1b3290a84ea5ecbc56ca08ee0452547
Uvindu Perera <uvinduperera822@gmail.com>
2026-08-13 14:31:16 +0530

    a

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://daring-perfection-production-fbe1.up.railway.app/api'
+  apiUrl: 'http://daring-perfection-production-fbe1.up.railway.app/api'
 };
